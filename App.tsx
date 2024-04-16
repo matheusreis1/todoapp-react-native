3a7e114e69a8398e2f2b81336268bbf7f6826722
Matheus Reis <theus.h.ass.reis@gmail.com>
2024-04-15 23:52:50 -0300

    touchable opacity with btn

--- App.tsx
+++ App.tsx
@@ -1,67 +1,20 @@
-import { useState } from 'react';
-import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
-
-interface Item {
-  id: string;
-  title: string; 
-}
-
-const MarginWrapper = ({children}: {children: React.ReactNode}) => (
-  <View style={{ margin: 4 }}>
-    {children}
-  </View>
-)
-
-const withMargin = (components: React.ReactNode[]) => components.map((component, index) => 
-  <MarginWrapper key={index}>{component}</MarginWrapper>
-);
-
-const Task = ({title, onButtonPress}: {title: string, onButtonPress: () => void}) => {
-  const components = [
-    <Text>{title}</Text>,
-    <Button title='Done' onPress={onButtonPress} />,
-    <Button title='Remove' onPress={onButtonPress} />
-  ];
-
-  return (
-    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-      {withMargin(components)}
-    </View>
-  );
-}
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 
 export default function App() {
-  const [currentTask, setCurrentTask] = useState('');
-  const [tasks, setTasks] = useState<Item[]>([]);
-  const handleOnButtonPress = ({title: titleToRemove}: Item) => {
-    const newTasks = tasks.filter(({title}) => title !== titleToRemove);
-    setTasks(newTasks);
+  const handleOnButtonPress = () => {
+    console.log('Botão pressionado!');
   }
 
   return (
     <View style={styles.container}>
       <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <TextInput 
-          style={styles.input}
-          placeholder="Enter a task" 
-          value={currentTask}
-          onChangeText={(text) => setCurrentTask(text)} />
-        <Button 
-          title='Add'
-          disabled={!currentTask}
-          onPress={() => {
-            const newTask = {title: currentTask, id: currentTask};
-            setTasks([...tasks, newTask])
-            setCurrentTask('');
-          }}
-        />
-      </View>
-
-      <FlatList 
-        data={tasks}
-        renderItem={({item}) => <Task title={item.title} onButtonPress={() => handleOnButtonPress(item)}></Task>}
-        keyExtractor={item => item.id}
-      ></FlatList>      
+        <TouchableOpacity 
+          style={{ padding: 10, backgroundColor: 'blue', borderRadius: 5 }} 
+          onPress={handleOnButtonPress}
+        >
+          <Text style={{ color: 'white' }}>Pressione-me!</Text>
+        </TouchableOpacity>
+      </View>     
     </View>
   );
 }
